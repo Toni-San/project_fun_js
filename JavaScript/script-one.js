2d9a2a662c079bda85404aa79b19b7d136fa74d8
Toni-San <derevko.dybovoe@gmail.com>
2018-10-28 01:06:25 +0300

    28102018h

--- JavaScript/script-one.js
+++ JavaScript/script-one.js
@@ -1,3 +1,6 @@
 // JavaScript Document
-alert('Я - JavaScript!');
-document.write('<h1>Я - JavaScript!</h1>')
+//alert('Я - JavaScript!');
+//document.write('<h1>Я - JavaScript!</h1>');
+//var x=5;
+//var y=9;
+//document.write(y+x);
