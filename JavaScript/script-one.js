f539698ac1a0c192e0422866385c36c0cc108275
Toni-San <derevko.dybovoe@gmail.com>
2018-11-08 17:53:39 +0300

    08112018w

--- JavaScript/script-one.js
+++ JavaScript/script-one.js
@@ -1,27 +1,3 @@
-// JavaScript Document
 "use strict";
-//alert('Я - JavaScript!');
-//document.write('<h1>Я - JavaScript!</h1>');
-//var x=5;
-//var y=9;
-//document.write(y+x);
-/*var message = 'Hello!';
-var user = 'John',
-	age = 25,
-	message = 'Hello';
-alert(message);
-alert(user);
-alert(age);
-alert(message);*/
-/*var a='hello';
-var b=a;
-alert(b);*/
-/*var admin;
-var name;
-name = 'Василий';
-admin = name;
-alert(name);*/
-/*var dor=0;
-var gor=10;
-var dor=gor;
-alert(dor);*/
+var planet = "Земля", name = "Петя";
+document.write(planet, name);
