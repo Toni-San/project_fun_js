602c3d00059683c607d050ebc356b5ed38ae04b6
Toni-San <C:\Users\OTK\AppData\Roaming\The Bat!>
2018-12-21 17:19:53 +0300

    21122018w1719

--- JavaScript/script-one.js
+++ JavaScript/script-one.js
@@ -1,6 +1,30 @@
 'use strict';
-var name = prompt('You naeme pleas?', 'Name')
-alert('You ' + name + ' LOL' );
-/*var years = prmpt('Сколько вам лет?', 100);
+
+/*Взаимодействие с пользователем: alert, prompt, confirm*/
+
+/*var years = prompt("Сколько вам лет?");
+alert(years);*/
+
+/*var years = prompt('Сколько вам лет?', 100);
 alert('Вам ' + years + ' лет!')*/
-//15112018w17:40
+
+/*var isAdmin = confirm("Вы - администратор?");
+alert( isAdmin );*/
+
+/*var name = prompt("Как вас зовут?", "");
+alert("Ваше имя " + name + "Верно?");*/
+
+/*Условные операторы: if, '?'*/
+
+/*var year = prompt('В каком году появилась спецификация ECMA-262 5.1?', '');
+if (year != 2011){
+	alert( 'А вот и неправильно!' );
+}*/
+
+var year = prompt('Введите год появления стандарта ECMA-262 5.1', '');
+
+if (year == 2011) {
+  alert( 'Да вы знаток!' );
+} else {
+  alert( 'А вот и неправильно!' ); // любое значение, кроме 2011
+}
