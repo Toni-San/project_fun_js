ef9b96b19583edaa42fbbfb34b83910af2921b24
Toni-San <C:\Users\OTK\AppData\Roaming\The Bat!>
2018-12-25 17:58:57 +0300

    25122018w1758

--- JavaScript/script-one.js
+++ JavaScript/script-one.js
@@ -21,10 +21,24 @@ if (year != 2011){
 	alert( 'А вот и неправильно!' );
 }*/
 
-var year = prompt('Введите год появления стандарта ECMA-262 5.1', '');
+/*var year = prompt('Введите год появления стандарта ECMA-262 5.1', '');
 
 if (year == 2011) {
   alert( 'Да вы знаток!' );
 } else {
   alert( 'А вот и неправильно!' ); // любое значение, кроме 2011
+}*/
+/*var question = prompt("«Каково «официальное» название JavaScript?»", "");
+if (question == "«ECMAScript»") {
+	alert("«Верно!»");
+}else {
+	alert("«Не знаете? «ECMAScript»!»");
+}*/
+var value = prompt("Введите значение!", "");
+if (value > 0) {
+	alert ( 1 );
+} else if (value < 0) {
+	alert ( -1 );
+} else {
+	alert ( 0 );
 }
