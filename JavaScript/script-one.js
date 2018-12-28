60f4f68e44a8748ce2a70a91b7aa98841caa2fce
Toni-San <C:\Users\OTK\AppData\Roaming\The Bat!>
2018-12-28 17:14:59 +0300

    28122018w1714

--- JavaScript/script-one.js
+++ JavaScript/script-one.js
@@ -111,3 +111,71 @@ nextPrime:
     alert( i ); // простое
   }
 */
+/*var age = prompt("Введите число", "");
+if (age >= 14 && age <= 90) {
+	alert("Верно");
+} else {
+	alert ("No");
+}*/
+ /*
+ var value = prompt("Каково «официальное» название JavaScript?","");
+ if (value == "ECMAScript") {
+ 	alert("Верно!");
+ } else {
+ 	alert ("Не знаете?«ECMAScript»!");
+ }
+ */
+ /*
+  var value = prompt("Введите значение!","");
+  if (value > 0) {
+  	alert( 1 );
+  } else if (value < 0) {
+  	alert( -1 );
+  } else {
+  	alert( 0 );
+  }
+*/
+/*
+var logIn = prompt("Введите логин","");
+
+console.log(logIn);
+
+if (logIn == "Админ") {
+		var password = prompt("Введите пароль","");
+		if (password == "Чёрный Властелин") {
+			alert("Добро пожаловать!");
+		} else if (password != "Чёрный Властелин") {
+			alert("Пароль неверен");
+		} else {
+			alert("Вход отменён");
+		};
+} else if ( logIn != "Админ" || logIn) {
+		alert("Я вас не знаю");
+} else {
+		alert("Вход отменён");
+};
+*/
+/*
+var userName = prompt('Кто пришёл?', '');
+
+if (userName == 'Админ') {
+
+  var pass = prompt('Пароль?', '');
+
+  if (pass == 'Чёрный Властелин') {
+    alert( 'Добро пожаловать!' );
+  } else if (pass == null) { // (*)
+    alert( 'Вход отменён' );
+  } else {
+    alert( 'Пароль неверен' );
+  }
+
+} else if (userName == null) { // (**)
+  alert( 'Вход отменён' );
+
+} else {
+
+  alert( 'Я вас не знаю' );
+
+}
+*/
