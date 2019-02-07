e05ad219a4591051badabcca7a22a3a1a68cd011
Toni-San <derevko.dybovoe@gmail.com>
2019-02-07 16:40:36 +0300

    07 02 2019 16 40

--- JavaScript/my.Script.js
+++ JavaScript/my.Script.js
@@ -1,1 +1,39 @@
 "use strict";
+// типы данных
+/*
+var str = 'Halk', //строка
+    number = 1, //число (простое)
+    numberSecond = 2.4, //число (дробное)
+    bool = true, //булевые выражения
+    undef, //undefined - значение не задано
+    nuull = null, // ничего
+    obj = {
+        prop: 'yo',
+        prop: function () {
+            
+        }
+    }; //обект
+alert(obj);
+*/
+//Примитивные и объектные данные. Обертки
+var str = 'Halk',
+    number = 1,
+    numberSecond = 2.4,
+    bool = true,
+    undef,
+    nuull = null;
+var obj = {
+        prop: 'yo',
+        prop: function () {
+
+        }
+    },
+    regexp = /baba/g,
+    func = function () {
+        
+    },
+    arr = [1,2,3];
+    console.log(typeof obj);
+    console.log(typeof regexp);
+    console.log(typeof func);
+    console.log(typeof arr);
