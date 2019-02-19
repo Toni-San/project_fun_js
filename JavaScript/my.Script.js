3e30a6bffdda41b12dc99c2a39732eb387911618
Toni-San <derevko.dybovoe@gmail.com>
2019-02-19 17:02:17 +0300

    19 02 2019 17 02

--- JavaScript/my.Script.js
+++ JavaScript/my.Script.js
@@ -15,6 +15,7 @@ var str = 'Halk', //строка
     }; //обект
 alert(obj);
 */
+/*
 //Примитивные и объектные данные. Обертки
 var str = 'Halk',
     number = 1,
@@ -37,3 +38,49 @@ var obj = {
     console.log(typeof regexp);
     console.log(typeof func);
     console.log(typeof arr);
+    */
+//Statements (инструкции), expressions (выражения), operators (операторы)
+//условные и логические операторы
+/*
+var num1 = null,
+    num2 = 2,
+    num3 = 3,
+    num4 = 4;
+*/
+//console.log(true + false);
+/*
+var age = prompt("Введите ваш возраст");
+if (age >=14 && age<=90){
+    alert("Guut!")
+} else {
+    alert("nicht!")
+};*/
+/*
+
+var i = 0;
+while (i <= 3) {
+    alert( i );
+    i++;
+}*/
+//код подсчитывает сумму вводимых чисел до тех пор, пока посетитель их вводит, а затем – выдаёт
+/*
+
+var sum = 0;
+
+while (true) {
+
+    var value = +prompt("Введите число", '');
+
+    if (!value) break; // (*)
+
+    sum += value;
+
+}
+alert( 'Сумма: ' + sum );
+*/
+/*
+for (var i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        alert( i );
+    }
+}*/
